0f12337bebd020c482440fa62a711cfae8b3509b
Alex Digrazia <alexndigrazia@hotmail.com>
2023-07-05 22:35:57 -0700

    FunctionalTrafficLight component completed

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
+
+type Colors = {
+  red: string;
+  yellow: string;
+  green: string;
+  [key: string]: string;
+};
+
 export const FunctionalTrafficLight = () => {
+  const [color, colorSetter] = useState("red");
+  const nextColor: Colors = {
+    red: "yellow",
+    yellow: "green",
+    green: "red",
+  };
+
+  const colorFilter = (filter: string) => (color === filter ? filter : "black");
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${colorFilter("red")}`}></div>
+        <div className={`circle ${colorFilter("yellow")}`}></div>
+        <div className={`circle ${colorFilter("green")}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button
+        className="next-state-button"
+        onClick={() => colorSetter(nextColor[color])}
+      >
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
