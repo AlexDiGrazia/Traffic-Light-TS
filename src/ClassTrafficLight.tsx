713509ee8ffeb513e7389718288af6cbfc0e91b4
Alex Digrazia <alexndigrazia@hotmail.com>
2023-07-06 13:24:26 -0700

    ClassTrafficLight component complete.  Also changed colorFilter() to lightOnOrOff() in both components

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,40 @@
 import { Component } from "react";
 
+type Colors = {
+  red: string;
+  yellow: string;
+  green: string;
+  [key: string]: string;
+};
+
+const nextColor: Colors = {
+  red: "green",
+  yellow: "red",
+  green: "yellow",
+};
+
 export class ClassTrafficLight extends Component {
+  state = { color: "red" };
+
+  lightOnOrOff = (filter: string) =>
+    this.state.color === filter ? filter : "black";
+
   render() {
+    const { color } = this.state;
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${this.lightOnOrOff("red")}`}></div>
+          <div className={`circle ${this.lightOnOrOff("yellow")}`}></div>
+          <div className={`circle ${this.lightOnOrOff("green")}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button
+          className="next-state-button"
+          onClick={() => this.setState({ color: nextColor[color] })}
+        >
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -8,23 +8,23 @@ type Colors = {
 };
 
 const nextColor: Colors = {
-  red: "yellow",
-  yellow: "green",
-  green: "red",
+  red: "green",
+  yellow: "red",
+  green: "yellow",
 };
 
 export const FunctionalTrafficLight = () => {
   const [color, colorSetter] = useState("red");
 
-  const colorFilter = (filter: string) => (color === filter ? filter : "black");
+  const lightOnOrOf = (filter: string) => (color === filter ? filter : "black");
 
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        <div className={`circle ${colorFilter("red")}`}></div>
-        <div className={`circle ${colorFilter("yellow")}`}></div>
-        <div className={`circle ${colorFilter("green")}`}></div>
+        <div className={`circle ${lightOnOrOf("red")}`}></div>
+        <div className={`circle ${lightOnOrOf("yellow")}`}></div>
+        <div className={`circle ${lightOnOrOf("green")}`}></div>
       </div>
       <button
         className="next-state-button"
